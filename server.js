efe10b2317fcd696313037bd902a612ffbb5d0d3
David Wright <davidjwright8@gmail.com>
2021-02-04 16:07:03 +0000

    Add URL validation

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const database = require('./database');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -29,6 +30,10 @@ app.listen(port, function() {
 
 app.post('/api/shorturl/new', (req, res) => {
   let originalUrl = req.body.url;
+  if (!validUrl.isUri(originalUrl)) {
+    res.json({error: 'invalid url'});
+    return;
+  }
   const urlAlias = database.saveToDatabase(originalUrl, (err) => {
     res.send(err);
   });
