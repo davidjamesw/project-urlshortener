0ec7dc166e7aa9a19e60f20b2afb5cf0f16b4518
David Wright <davidjwright8@gmail.com>
2021-02-04 22:43:49 +0000

    Change url validation method to a dns lookup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const database = require('./database');
 const bodyParser = require('body-parser');
-const validUrl = require('valid-url');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -30,21 +30,30 @@ app.listen(port, function() {
 
 app.post('/api/shorturl/new', (req, res) => {
   let originalUrl = req.body.url;
-  if (!validUrl.isUri(originalUrl)) {
-    res.json({ error: 'invalid url' });
-  } else {
-    const urlAlias = database.saveToDatabase(originalUrl, (err) => {
-      res.send(err);
-    });
-    res.json({original_url: originalUrl, short_url: urlAlias});
-  }
+  validateUrl(originalUrl, (addresses) => {
+    if (addresses) {
+      const urlAlias = database.saveToDatabase(originalUrl, (err) => {
+        res.send(err);
+      });
+      res.json({original_url: originalUrl, short_url: urlAlias});
+    } else {
+      res.json({ error: 'invalid url' });
+    }
+  });
 });
 
 app.get('/api/shortcut/:alias', (req, res) => {
   const url = database.getUrlFromDatabase(req.params.alias, (err, url) => {
     if (err) {
       res.send(err);
+      next();
     }
     res.redirect(url);
   });
 });
+
+function validateUrl(url, invalid) {
+  dns.lookup(url, {all: true}, (err, addresses) => {
+    invalid(addresses);
+  });
+}
