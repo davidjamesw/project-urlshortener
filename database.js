ec653f00fee37eb423ff146170518e3299708bb6
David Wright <davidjwright8@gmail.com>
2021-02-04 14:22:47 +0000

    Write url and alias to database
    Generate a shorter hash using crypto.randomBytes.
    
    Using mongoose, write the URL that has been provided to the database.

--- database.js
+++ database.js
@@ -2,9 +2,26 @@ const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 const crypto = require('crypto');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  url: {type: String, required: true},
+  alias: {type: String, required: true}
+})
+
+const Url = mongoose.model("Url", urlSchema);
+
 function saveToDatabase(url) {
-  const urlAlias = crypto.createHash('md5').update(url).digest('hex');
-  console.log(`Writing ${url} and ${urlAlias} to the database`);
+  let urlAlias = crypto.randomBytes(4).toString('hex');
+  let mappedUrl = new Url({"url": url, "alias": urlAlias});
+  console.log(`Writing ${url} to the database, with an alias of ${urlAlias}`);
+    mappedUrl.save(function(err, data) {
+      if (err) {
+        return console.error(err);
+      }
+    });
   return urlAlias;
 }
 
